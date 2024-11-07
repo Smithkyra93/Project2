84f74031dc594bf1da7c1b12a7f3de98aa3c78fa
Kyra Smith <kyrasmith@Kyras-Air.attlocal.net>
2024-11-06 20:32:47 -0600

    update

--- js/main.js
+++ js/main.js
@@ -10,15 +10,18 @@ async function generateGif() {
     let q = document.getElementById("search-box").value;
     //We need 10 gifs to be displayed in result
     let gifCount = 10;
-    let APIURL = "https://api.giphy.com/v1/gifs/search?api_key=6V20ado4NsUCs4H45eJ4b821HJuYmBkE&q=laugh&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+
+    let APIURL = `https://api.giphy.com/v1/gifs/search?api_key=6V20ado4NsUCs4H45eJ4b821HJuYmBkE&q=${q}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
     let response = await fetch (APIURL)
     let data = await response.json()
     console.log(data);
+    //Empty section with all images
+    let sectionElement = document.getElementById("gifImages")
+    sectionElement.innerHTML = ""
     data.data.forEach(element => {
         let newImage = document.createElement("img")
         newImage.src = element.images.original.url
-        console.log (newImage)
-        let sectionElement = document.getElementById("gifImages")
+        
         console.log (sectionElement)
         sectionElement.appendChild(newImage)
     });
